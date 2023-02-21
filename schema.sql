44e8a684fb4d3277cc6a0dc8d692f522b176f592
Abu Raihan <raihan2bd.official@gmail.com>
2023-02-21 14:18:13 +0600

    Modify animals id column

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id NOT NULL PRIMARY KEY,
+   id bigserial PRIMARY KEY,
     name  VARCHAR(100) NOT NULL,
     date_of_birth DATE,
     escape_attempts INT,
